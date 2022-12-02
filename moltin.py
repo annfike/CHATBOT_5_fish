6085b6f8a6304eb2173477506e9553911ec7ab05
Anna <44573387+annfike@users.noreply.github.com>
2022-12-02 17:49:01 +0300

    generating buttons from products

---
+++ moltin.py
@@ -0,0 +1,64 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def get_products(client_id):
+    data = {
+        'client_id': client_id,
+        'grant_type': 'implicit',
+    }
+    response = requests.post('https://api.moltin.com/oauth/access_token', data=data)
+    moltin_access_token = response.json()['access_token']
+    headers = {
+        'Authorization': f'Bearer {moltin_access_token}',
+    }
+
+    #response = requests.get('https://api.moltin.com/v2/carts/abc', headers=headers)
+    response = requests.get('https://api.moltin.com/v2/products', headers=headers)
+    return response.json()['data']
+
+
+def main() -> None:
+    load_dotenv()
+    client_id = os.getenv('MOLTIN_CLIENT_ID')
+    get_products(client_id)
+    
+
+    '''
+    #добавляем продукт в корзину по его id
+    headers = {
+        'Authorization': f'Bearer {moltin_access_token}',
+        'Content-Type': 'application/json',
+    }
+
+    json_data  = {
+        'data': {
+            'id': '2f0fdfa0-b9e5-4d66-962d-1673e84961f7',
+            'type': 'cart_item',
+            'quantity': 1,
+        },
+    }
+
+    response = requests.post('https://api.moltin.com/v2/carts/:reference/items', headers=headers, json=json_data)
+    print(response.json())
+
+
+    #выводим корзину 
+    headers = {
+        'Authorization': f'Bearer {moltin_access_token}',
+    }
+
+    response = requests.get('https://api.moltin.com/v2/carts/:reference', headers=headers)
+    print(response.json())
+    
+    #выводим список товаров в корзине
+    headers = {
+        'Authorization': f'Bearer {moltin_access_token}',
+    }
+
+    response = requests.get('https://api.moltin.com/v2/carts/:reference/items', headers=headers) 
+    print(response.json())
+    '''
+
+if __name__ == '__main__':
+    main()
--- tgbot.py
+++ tgbot.py
@@ -1,66 +1,114 @@
-import requests
 import os
+import logging
+import redis
 from dotenv import load_dotenv
+from moltin import get_products
+
+from telegram import ReplyKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (CallbackContext, CommandHandler, ConversationHandler,
+                          Filters, MessageHandler, Updater)
+from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
+                          MessageHandler, Updater)
+_database = None
 
 
-def main() -> None:
-    load_dotenv()
-    client_id = os.getenv('MOLTIN_CLIENT_ID')
+def start(update: Update, context: CallbackContext):
+    products = get_products(client_id)
+    keyboard = []
+    for product in products:
+        product_id = product['id']
+        product_name = product['name']
+        keyboard.append([InlineKeyboardButton(product_name, callback_data=product_id)])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(text='Привет', reply_markup=reply_markup)
+    return 'ECHO'
 
-    #получаем access token
-    data = {
-        'client_id': client_id,
-        'grant_type': 'implicit',
+
+def button(update, context):
+    products = get_products(client_id)
+    keyboard = []
+    for product in products:
+        product_id = product['id']
+        product_name = product['name']
+        keyboard.append([InlineKeyboardButton(product_name, callback_data=product_id)])
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text=f"Selected option: {query.data}",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+    
+
+def echo(update, context):
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return 'ECHO'
+
+
+def handle_users_reply(update, context):
+    """
+    Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
+    Эта функция запускается в ответ на эти действия пользователя:
+        * Нажатие на inline-кнопку в боте
+        * Отправка сообщения боту
+        * Отправка команды боту
+    Она получает стейт пользователя из базы данных и запускает соответствующую функцию-обработчик (хэндлер).
+    Функция-обработчик возвращает следующее состояние, которое записывается в базу данных.
+    Если пользователь только начал пользоваться ботом, Telegram форсит его написать "/start",
+    поэтому по этой фразе выставляется стартовое состояние.
+    Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
+    """
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
+    
+    states_functions = {
+        'START': start,
+        'ECHO': echo
     }
-    response = requests.post('https://api.moltin.com/oauth/access_token', data=data)
-    moltin_access_token = response.json()['access_token']
-    print(moltin_access_token)
+    state_handler = states_functions[user_state]
+    # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
+    # Оставляю этот try...except, чтобы код не падал молча.
+    # Этот фрагмент можно переписать.
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
 
+def get_database_connection():
+    """
+    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
+    """
+    global _database
+    if _database is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
 
-    '''
-    # получаем инфо о продуктах
-    headers = {
-        'Authorization': f'Bearer {moltin_access_token}',
-    }
-
-    #response = requests.get('https://api.moltin.com/v2/carts/abc', headers=headers)
-    response = requests.get('https://api.moltin.com/v2/products', headers=headers)
-    print(response.json())
-
-
-    #добавляем продукт в корзину по его id
-    headers = {
-        'Authorization': f'Bearer {moltin_access_token}',
-        'Content-Type': 'application/json',
-    }
-
-    json_data  = {
-        'data': {
-            'id': '2f0fdfa0-b9e5-4d66-962d-1673e84961f7',
-            'type': 'cart_item',
-            'quantity': 1,
-        },
-    }
-
-    response = requests.post('https://api.moltin.com/v2/carts/:reference/items', headers=headers, json=json_data)
-    print(response.json())
-
-
-    #выводим корзину 
-    headers = {
-        'Authorization': f'Bearer {moltin_access_token}',
-    }
-
-    response = requests.get('https://api.moltin.com/v2/carts/:reference', headers=headers)
-    print(response.json())
-    '''
-    #выводим список товаров в корзине
-    headers = {
-        'Authorization': f'Bearer {moltin_access_token}',
-    }
-
-    response = requests.get('https://api.moltin.com/v2/carts/:reference/items', headers=headers) 
-    print(response.json())
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    client_id = os.getenv('MOLTIN_CLIENT_ID')
+    token = os.getenv("TELEGRAM_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    
+    #dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    updater.start_polling()
